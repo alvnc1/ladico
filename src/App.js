a31818b8996b63fee31e50e9f8aa6975f470d596
Alan Cabezas <alan.cabezas@usm.cl>
2025-05-07 23:35:10 -0400

    navigate en homepage en App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginRegister from './components/LoginRegister';
 import Home from './components/Home';
 import HomePage from './components/HomePage';
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/homepage" />} />
         <Route path="/loginregister" element={<LoginRegister />} />
         <Route path="/home" element={<Home />} />
         <Route path="/homepage" element={<HomePage />} />
