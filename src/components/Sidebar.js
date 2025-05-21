64fe9b68e1c75db398a7e3b20c5d1a79da069b49
Alan Cabezas <alan.cabezas@usm.cl>
2025-05-20 21:11:17 -0400

    reac icons para Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import '../style/Sidebar.css';
+import { FaHome, FaStar, FaCertificate, FaBookOpen, FaQuestionCircle } from 'react-icons/fa';
 
 function Sidebar({ onNavigate, currentPage }) {
-
   return (
     <aside className="sidebar">
-  <img src="/img/ladico.png" alt="LADICO logo" className="logo" />
-  <nav>
-    <ul>
-      <li
-        className={currentPage === 'competencias' ? 'active' : ''}
-        onClick={() => onNavigate('competencias')}
-      >
-        Competencias
-      </li>
-      <li
-        className={currentPage === 'progreso' ? 'active' : ''}
-        onClick={() => onNavigate('progreso')}
-      >
-        Mi progreso
-      </li>
-      <li
-        className={currentPage === 'explorar' ? 'active' : ''}
-        onClick={() => onNavigate('explorar')}
-      >
-        Explorar
-      </li>
-    </ul>
-  </nav>
-</aside>
+      <div className="logo-section">
+        <img src="/img/ladico.png" alt="LADICO logo" className="logo" />
+      </div>
 
+      <nav className="nav-links">
+        <ul>
+          <li
+            className={currentPage === 'home' ? 'active' : ''}
+            onClick={() => onNavigate('home')}
+          >
+            <FaHome /> Inicio
+          </li>
+          <li
+            className={currentPage === 'competencias' ? 'active' : ''}
+            onClick={() => onNavigate('competencias')}
+          >
+            <FaStar /> Competencias
+          </li>
+          <li
+            className={currentPage === 'progreso' ? 'active' : ''}
+            onClick={() => onNavigate('progreso')}
+          >
+            <FaCertificate /> Progreso
+          </li>
+          <li
+            className={currentPage === 'explorar' ? 'active' : ''}
+            onClick={() => onNavigate('explorar')}
+          >
+            <FaBookOpen /> Explorar
+          </li>
+        </ul>
+      </nav>
+
+    </aside>
   );
 }
 
