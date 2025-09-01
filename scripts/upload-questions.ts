d111f46d7be143322715ed3213582fc96dc46f17
Alan Cabezas <alan.cabezas@usm.cl>
2025-09-01 01:41:12 -0400

    area 5 funcional

--- scripts/upload-questions.ts
+++ scripts/upload-questions.ts
@@ -8,7 +8,7 @@ import { firebaseConfig } from "../lib/firebase";
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
-const competenceCodes = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "3.1", "3.2", "3.3", "3.4", "4.1", "4.2", "4.3", "4.4"]
+const competenceCodes = ["1.1", "1.2", "1.3", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "3.1", "3.2", "3.3", "3.4", "4.1", "4.2", "4.3", "4.4","5.1", "5.2", "5.3", "5.4"]
 
 async function checkQuestions() {
   try {
