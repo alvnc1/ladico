86fe503a20e8f625ff5b891bd6e2f6629f8a9f6a
Alan Cabezas <alan.cabezas@usm.cl>
2025-09-09 04:13:26 -0300

    migrado de bd

--- scripts/firebase/upload-simple.js
+++ scripts/firebase/upload-simple.js
@@ -22,14 +22,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAahNL2-uxj6wOGieWXdDUvcEx9Gdka-a0",
-  authDomain: "ladico-3eef2.firebaseapp.com",
-  projectId: "ladico-3eef2",
-  storageBucket: "ladico-3eef2.firebasestorage.app",
-  messagingSenderId: "622858666638",
-  appId: "1:622858666638:web:f512807a2b6550f59d3fdf",
-  measurementId: "G-HB4GCM2JX3"
-};
+  apiKey: "AIzaSyD0nNdAafmNc4Crejel9eSl57a8tOpnsvI",
+  authDomain: "ladico-d8aa2.firebaseapp.com",
+  projectId: "ladico-d8aa2",
+  storageBucket: "ladico-d8aa2.firebasestorage.app",
+  messagingSenderId: "79757424410",
+  appId: "1:79757424410:web:79247603cf5f70d9939ea3",
+  measurementId: "G-2K90VT76EC"
+}
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
